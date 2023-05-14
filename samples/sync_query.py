9e8b012d287260488aff996d43106e0a0e9bc06c
BrianOSu <b.osullivan@live.ie>
2023-05-14 14:41:56 +0100

    Revert sync_query.py changes

--- samples/sync_query.py
+++ samples/sync_query.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     # low-level query and read
     q.query(qconnection.MessageType.SYNC, '{`short$ til x}', 10) # sends a SYNC query
     msg = q.receive(data_only=False, raw=False) # retrieve entire message
+    print('type: %s, message type: %s, data size: %s, is_compressed: %s ' % (type(msg), msg.type, msg.size, msg.is_compressed))
     data = msg.data
     print('type: %s, numpy.dtype: %s, meta.qtype: %s, data: %s ' % (type(data), data.dtype, data.meta.qtype, data))
     # close connection
